9f30bfd5c5bb17d045402d21f1c92f553daa5cfe
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-01-20 16:46:05 +0100

    Removing the inappropriate normalizing in the training loop

--- cryosphere/cryosphere_train.py
+++ cryosphere/cryosphere_train.py
@@ -69,7 +69,7 @@ def start_training(vae, image_translator, ctf, grid, gmm_repr, optimizer, datase
             predicted_structures = model.utils.deform_structure(gmm_repr.mus, translation_per_residue, quaternions_per_domain, segmentation, gpu_id)
             posed_predicted_structures = renderer.rotate_structure(predicted_structures, batch_poses)
             predicted_images  = renderer.project(posed_predicted_structures, gmm_repr.sigmas, gmm_repr.amplitudes, grid)
-            batch_predicted_images = renderer.apply_ctf(predicted_images, ctf, indexes)/dataset.f_std
+            batch_predicted_images = renderer.apply_ctf(predicted_images, ctf, indexes)#/dataset.f_std
             loss = compute_loss(batch_predicted_images, lp_batch_translated_images, None, latent_mean, latent_std, vae.module, segmenter.module, experiment_settings, tracking_metrics, 
                 structural_loss_parameters= structural_loss_parameters, epoch=epoch, predicted_structures=predicted_structures, device=gpu_id)
 
