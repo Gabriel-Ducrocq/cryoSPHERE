4a87d424c78270c1cb8a366c8ee286d910c69309
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-01-21 16:47:31 +0100

    Fixing argument order issues in start_sample_latent

--- cryosphere/data/analyze.py
+++ cryosphere/data/analyze.py
@@ -133,7 +133,7 @@ def graph_traversal(z_pca, dim, num_points=10):
     traj_pca[:, dim] = np.linspace(start, stop, num_points)
     return traj_pca
 
-def start_sample_latent(rank, backbone_network, all_heads, world_size,  yaml_setting_path, output_path, model_path, segmenter_path, num_workers=4):
+def start_sample_latent(rank, world_size,  backbone_network, all_heads, yaml_setting_path, output_path, model_path, segmenter_path, num_workers=4):
     utils.ddp_setup(rank, world_size)
     (vae, backbone_network, all_heads, image_translator, ctf_experiment, grid, gmm_repr, optimizer, dataset, N_epochs, batch_size, experiment_settings, device,
     scheduler, base_structure, lp_mask2d, mask, amortized, path_results, structural_loss_parameters, segmenter)  = utils.parse_yaml(yaml_setting_path, rank, analyze=True)
