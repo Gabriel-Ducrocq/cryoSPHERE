5d545a37171007f37444155cba2c88c92dde13e8
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-05-12 13:12:46 +0200

    Fixing bug: pretraining only if no checkpoint loaded

--- cryosphere/cryosphere_train.py
+++ cryosphere/cryosphere_train.py
@@ -42,7 +42,7 @@ def start_training(vae, image_translator, ctf, grid, gmm_repr, optimizer, datase
     base_structure, lp_mask2d, mask_images, amortized, path_results, structural_loss_parameters, segmenter, gpu_id):
     vae = DDP(vae, device_ids=[gpu_id])
     segmenter = DDP(segmenter, device_ids=[gpu_id])
-    if experiment_settings["resume_training"]["model"] is not None:
+    if experiment_settings["resume_training"]["model"] is None:
         pretrain(vae, dataset, experiment_settings, gpu_id)
 
     for epoch in range(N_epochs):
